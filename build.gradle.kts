eb04f45bb769b2423a889d1c0305ad4165229ea2
pranga09 <pranga.ven@parkland.ca>
2025-06-20 00:05:07 -0400

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    id("org.jetbrains.compose") version "1.5.0"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    js(IR) {
+        browser {
+            binaries.executable()
+            runTask {}
+        }
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+            }
+        }
+    }
+}
+
+compose.experimental {
+    web.application {}
+}
